c423f3b6c7d8933eb80650295260971b385a279a
HolyPi <joanelly2018@gmail.com>
2020-03-02 16:31:23 -0800

    test

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', 'makewiki-jc.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wiki',
-    'accounts',
-    'rest_framework'
+    'accounts'
+    
 ]
 
 MIDDLEWARE = [
