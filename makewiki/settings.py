e0ee20d22284899e0dca78f37b9335682eb051ab
HolyPi <joanelly2018@gmail.com>
2020-03-02 16:17:18 -0800

    tweaks for deployment

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com']
 
 
 # Application definition
@@ -140,10 +140,12 @@ LOGOUT_REDIRECT_URL = "/"
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+
 # PROTIP:
 # Need to override settings? Create a local_settings.py file
 # in this directory, and add settings there.
-try:
-    from makewiki.local_settings import *
-except ImportError:
-    pass
+# try:
+#     from makewiki.local_settings import *
+# except ImportError:
+#     pass
